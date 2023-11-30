7eb36083fb109597b96bd9a6c54893124cf2ae7c
kaualira <130668344+kaualira@users.noreply.github.com>
2023-11-30 13:28:55 -0300

    kl

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
   include_once("templates/header.php");
 ?>
-</body>
-</html>
+<div class="plano-de-fundo">
+  <video autoplay loop muted plays-inline class="back-video">
+      <source src="imgs/ararazoo.mp4" type="video/mp4">
+  </video>
+</div>
