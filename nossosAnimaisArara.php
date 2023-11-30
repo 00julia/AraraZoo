5d45936a89ba3bc275ede3abd6d7e80714585e34
00julia <130668482+00julia@users.noreply.github.com>
2023-11-30 11:58:30 -0300

    julia m

---
+++ nossosAnimaisArara.php
@@ -0,0 +1,12 @@
+<?php
+include_once("templates/header.php"); 
+?>
+<link rel="stylesheet" href="css/nossosAnimaisArara.css">
+
+<div class="container">
+<img src="imgs/nossosanimais.jpg" class="img1NA">
+</div>
+
+<div class="container2">
+<img src="imgs/emextinção.jpg" class="img2NA">
+</div>
