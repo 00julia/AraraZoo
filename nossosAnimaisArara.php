6cdd99220eb693d2dbf026c1a59d9171e0082810
00julia <130668482+00julia@users.noreply.github.com>
2023-11-30 14:10:59 -0300

    julia mogi

--- nossosAnimaisArara.php
+++ nossosAnimaisArara.php
@@ -7,6 +7,17 @@ include_once("templates/header.php");
 <img src="imgs/nossosanimais.jpg" class="img1NA">
 </div>
 
+<!-- em extinção -->
 <div class="container2">
 <img src="imgs/emextinção.jpg" class="img2NA">
 </div>
+
+<div class="container3">
+<img src="imgs/emExtinçao2.png" class="img3NA">
+</div>
+
+<!-- resgatados -->
+<div class="container4">
+<img src="imgs/resgatados.jpg" class="img4NA">
+</div>
+
