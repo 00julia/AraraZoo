d70ae8703b3accaf73254f219026bfcd209c6089
00julia <130668482+00julia@users.noreply.github.com>
2023-11-30 09:45:02 -0300

    Update nossosProjetosArara.php

--- nossosProjetosArara.php
+++ nossosProjetosArara.php
@@ -2,11 +2,33 @@
 // include_once("AraraZoo/imagens/nosoprojeto.jpg"); 
 ?>
 <html>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Jura:wght@300&display=swap" rel="stylesheet">
+
 <img src="imagens/nossoprojeto.jpg" class="imgNP">
+
+<p class="pNP"> O AraraZoo nasceu como uma nova opção de entretenimento para a família juntamente com algo sustentável, que seja inofensivo a natureza e que possa mudar o mundo. Graças a isso, o AraraZoo veio para inovar o mercado de viagens/entretenimento com seu Hotel Zoológico que, além de proporcionar experiencias únicas com contato direto a natureza, o AraraZoo tem como seu principal projeto a preservação de animais silvestres que estão ameaçados de extinção, os mantendo em santuários e os deixando em segurança, para que possam procriar e serem soltos na natureza. 
+ Além disso, o AraraZoo preza pela segurança de animais maltratados ou explorados pelo homem, resgatando esses bichos, e fazendo de tudo para que o mesmo se sinta bem de novo e preparado para a natureza.</p>
 
 </html>
 <style>
-.imgNP{
     
+.imgNP{
+  
+    display: block; 
+    margin-left: auto; 
+    margin-right: auto;
+    width: 50%; 
+    height: auto;
+}
+.pNP{
+    display: block; 
+    margin-left: auto; 
+    margin-right: auto;
+    width: 50%; 
+    height: auto;
+    font-size: 120%;
+    font-family: 'Jura', sans-serif;
 }
     </style>
