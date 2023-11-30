061b3eacf43946e4061b0e1cd6d4b6dd94c1c1da
00julia <130668482+00julia@users.noreply.github.com>
2023-11-30 08:58:28 -0300

    Update nossosProjetosArara.php

--- nossosProjetosArara.php
+++ nossosProjetosArara.php
@@ -1,3 +1,12 @@
 <?php
-include_once("AraraZoo/imagens/nosoprojeto.jpg"); 
+// include_once("AraraZoo/imagens/nosoprojeto.jpg"); 
 ?>
+<html>
+<img src="imagens/nossoprojeto.jpg" class="imgNP">
+
+</html>
+<style>
+.imgNP{
+    
+}
+    </style>
