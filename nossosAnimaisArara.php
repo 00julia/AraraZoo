c6433bff6a5ce626dff5d04c2871f28eb0b61479
00julia <130668482+00julia@users.noreply.github.com>
2023-12-01 08:55:46 -0300

    julia p

--- nossosAnimaisArara.php
+++ nossosAnimaisArara.php
@@ -21,9 +21,17 @@ include_once("templates/header.php");
 <img src="imgs/resgatados.jpg" class="img4NA">
 </div>
 
-<div class="container5">
-<img src="imgs/resgatados.jpg" class="img5NA">
-<p class="arara1">Araras</p>
-<p class="arara2">Nosso ambiente das araras conta com mais de 10 aves resgatadas de tráfico de animais e/ou machucadas que não conseguem mais ser reecolocadas na natureza.</p>
+
+<div class="margin-wrapper">
+<div class="container">
+    <div class="left-content">
+        <img src="sua-imagem.jpg" alt="Imagem na esquerda">
+    </div>
+    <div class="right-content">
+        <h1>Araras</h1>
+        <p>Nosso ambiente das araras conta com mais de 10 aves resgatadas de tráfico de animais e/ou machucadas que não conseguem mais ser reecolocadas na natureza.</p>
+    </div>
 </div>
+</div>
+
 
