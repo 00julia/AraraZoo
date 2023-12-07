9737c90bb5af9b1d8e205cebafa074dddbbf717a
00julia <130668482+00julia@users.noreply.github.com>
2023-12-07 14:05:18 -0300

    juju

--- login.php
+++ login.php
@@ -1,14 +1,15 @@
 <?php
     include_once("templates/header.php"); 
 ?>
+
 <link rel="stylesheet" href="css/login.css">
 
-<body>
+
     <div class="login-container">
-        <h2></h2>
+        <!-- <h2></h2> -->
         <form action="/login" method="post">
         <div class="imagem">
-    <img src="login.png" width="320" text-align=center; alig height="205" />
+    <img src="imgs/login.png" width="320" text-align=center; alig height="205" />
 </div>
             <input type="text" name="username" placeholder="digite seu email..." required>
             <br>
@@ -17,5 +18,4 @@
             <button type="submit">ENTRAR</button>
         </form>
     </div>
-</body>
-</html>
+
