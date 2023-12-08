9c621c0f2ca449a3b622ef87a51e01c1ffe49333
kaualira <130668344+kaualira@users.noreply.github.com>
2023-12-08 08:56:20 -0300

    kl

---
+++ quartocasal.php
@@ -0,0 +1,15 @@
+<?php
+    include_once("templates/header.php"); 
+?>
+<link rel="stylesheet" href="css/quartocasal.css">
+<div class="quartocasal">
+    <div class="quartocasalfoto">
+        <img src="imgs/3.jpg" width="700px">
+    </div>
+    <div class="quartocasalfoto">
+        <img src="imgs/quartocasal.jpg" class="fotodoquarto">
+    </div>
+    <div class="quartocasalfoto">
+        <img src="imgs/pacotefamilia.png" class="fotodoprecodoquarto">
+    </div>
+</div>
