233557b7cb8d3b0409b9ed575e30e4b8ca8adf65
kavs1346 <130669072+kavs1346@users.noreply.github.com>
2023-12-08 09:14:58 -0300

    (⋋▂⋌)

--- quartofamilia.php
+++ quartofamilia.php
@@ -4,13 +4,13 @@
 
 <link rel="stylesheet" href="css/quartofamilia.css">
 <br>
-<br>
-<br>
-<br>
 <div>
     <img src="imgs/4.jpg" class="imgfamilia">
 </div>
-
 <div>
     <img src="imgs/quartofamilia.jpg" class="imgcama">
+</div>
+<br>
+<div>
+    <img src="imgs/pacotefamilia.png" class="imgpreço">
 </div>
